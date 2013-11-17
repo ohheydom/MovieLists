96644d023800c8aefcf9958ca8494ef72351b12c
Dominic Leone <ohheydom@gmail.com>
2013-11-17 15:24:19 -0500

    Authorization done

--- app/models/profile.rb
+++
@@ -1,4 +0,0 @@
-class Profile < ActiveRecord::Base
-has_many :connectors
-has_many :movies, through: :connectors
-end
--- app/views/profiles/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @profile, :name, :genre, :created_at, :updated_at
