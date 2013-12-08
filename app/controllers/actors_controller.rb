11ce27c54ed6e789dc098ddc0d4b237a67f84712
Dominic Leone <ohheydom@gmail.com>
2013-12-07 22:16:56 -0500

    refactoring

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -19,70 +19,24 @@ helper_method :get_actors, :get_actors_name, :ive_seen_it
   end
   
   def destroy
-  @connector = Connector.find_by_user_id_and_movie_id(current_user.id, params[:movie_id])
-  if @connector.present?
-  @connector.destroy
-  end
-  
-  respond_to do |format|
-				format.html { redirect_to actor_path(@actor) }
-				#format.json { render json: @movie }
-				format.js 
-    end
-	
-	
+	path = actor_path(@actor)
+	super
   end
   
   
   def create
-	@movie = Movie.new(movie_params)
-	@connector = Connector.new(connector_params)
-	@movie.save
-			#respond_to do |format|
-			#if @movie.save
-			#	format.html { redirect_to actor_path(@actor) }
-			#	format.json { render json: @movie }
-			#	format.js
-			#else
-			#	format.html { redirect_to _path(@actor) }
-			#	format.json { render json: @movie.errors }
-			#	format.js
-			#end
-		#end
-	@connector.save	
-		respond_to do |format|
-				format.html { redirect_to actor_path(@actor) }
-				#format.json { render json: @movie }
-				format.js
-
-		end
+	path = actor_path(@actor)
+	super
   end
   
   def update
-	@oldmovie = Movie.find(params[:movie_id])
-	if @oldmovie.present?
-		@oldmovie.update(movie_params)
-		   respond_to do |format|
-			if @oldmovie.update(movie_params)
-				format.html { redirect_to actor_path(params[:id]) }
-				format.js
-			else
-				format.html { redirect_to actor_path(params[:id]) }
-				format.js
-			end
-		   end
-	end
+	path = actor_path(params[:id])
+	super
   end
 
 
 
-	def movie_params
-		params.permit(:id, :title, :actors, :year).merge(:id => params[:movie_id], :actors => get_actors(params[:movie_id]), :year => params[:year])
-    end
-	
-	def connector_params
-         params.permit(:user_id, :movie_id).merge(:user_id => current_user.id)
-    end
+
 	  
  	def get_actors(movie_id)
 		ary = Tmdb::TheMovieDb.get_movie_credits_by_movie_id(movie_id)['cast']
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,69 @@ class ApplicationController < ActionController::Base
   profile_path(current_user)
   end
 
+  
+  #Shared between actors_controller, lists_controller, and movies_controller
+  
+  def create
+	@movie = Movie.new(movie_params)
+	@connector = Connector.new(connector_params)
+	@movie.save
+			#respond_to do |format|
+			#if @movie.save
+			#	format.html { redirect_to actor_path(@actor) }
+			#	format.json { render json: @movie }
+			#	format.js
+			#else
+			#	format.html { redirect_to _path(@actor) }
+			#	format.json { render json: @movie.errors }
+			#	format.js
+			#end
+		#end
+	@connector.save	
+		respond_to do |format|
+				format.html { redirect_to path }
+				#format.json { render json: @movie }
+				format.js
+
+		end
+  end
+  
+  def destroy 
+	@connector = Connector.find_by_user_id_and_movie_id(current_user.id, params[:movie_id])
+	#if @connector.present?
+	@connector.destroy
+	#end
+		respond_to do |format|
+			format.html { redirect_to path }
+			#format.json { render json: @movie }
+			format.js 
+		end
+  end
+  
+  def update
+	@oldmovie = Movie.find(params[:movie_id])
+	if @oldmovie.present?
+		@oldmovie.update(movie_params)
+		   respond_to do |format|
+			if @oldmovie.update(movie_params)
+				format.html { redirect_to path }
+				format.js
+			else
+				format.html { redirect_to path }
+				format.js
+			end
+		   end
+	end
+  end
+  
+  	def movie_params
+		params.permit(:id, :title, :actors, :year).merge(:id => params[:movie_id], :actors => get_actors(params[:movie_id]), :year => params[:year])
+    end
+	
+	def connector_params
+         params.permit(:user_id, :movie_id).merge(:user_id => current_user.id)
+    end
+	
 protected
 
   def configure_permitted_parameters
