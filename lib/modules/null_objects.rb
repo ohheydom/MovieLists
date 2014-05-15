3f9e2b46a43e1be58b1a95bfa99ca6d33da7aa39
Dom Leone <ohheydom@gmail.com>
2014-05-15 15:03:13 -0400

    Cleaned up movies#show a bit more.

--- lib/modules/null_objects.rb
+++ lib/modules/null_objects.rb
@@ -9,4 +9,12 @@ module NullObjects
       '1900-01-01'
     end
   end
+
+  class NotSignedIn
+    FormData = Struct.new(:method, :submit, :trclass, :path)
+
+    def form_data
+      FormData.new('', '', 'noclass', '')
+    end
+  end
 end
